a04a23798802aee663346b2cbbe3f442bf07cb4a
nidaakb <nidaakabbaj1997@gmail.com>
2024-06-08 13:52:44 +0200

    fix

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,4 +1,4 @@
-const getPuzzle = async (wordCount) => {
+const getPuzzle = async function (wordCount) {
     const response = await fetch ('https://puzzle.mead.io/puzzle?wordCount=2');
     console.log(response);
     const data = await response.json();
